8bd850c1474d841efa66f3e9423897719d23f9b9
Mathias Escobar <mathias.escobar.jf@gmail.com>
2026-02-24 17:52:05 -0300

    Agregando SearchInput

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import SearchInput from "@/app/ui/SearchInput";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,6 +15,7 @@ function RootLayout({children}: {
       <body
         className={"antialiased"}
       >
+        <SearchInput />
         {children}
       </body>
     </html>
