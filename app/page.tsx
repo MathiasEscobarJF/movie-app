e42f721ad8928ee87b7b110f1d740d823117655c
Mathias Escobar <mathias.escobar.jf@gmail.com>
2026-02-24 17:18:17 -0300

    Reiniciando app

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 function Home(){
   return (
-    <div className="@container flex-1 flex flex-col bg-violet-950">
-        <h1 className="mx-auto ">Search for a movie or series to start</h1>
-        <h2 className="mx-auto ">Powered by the OMDb API - created by Mathias Escobar</h2>
-    </div>
+    <main>
+        <h1>Hello World!</h1>
+    </main>
   );
 }
 
