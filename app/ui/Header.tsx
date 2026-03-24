be4ffb540026896dc833ded00727875b1d10c5be
Mathias Escobar <mathias.escobar.jf@gmail.com>
2026-03-24 18:25:04 -0300

    Adding  Suspense to Header

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -1,14 +1,17 @@
 import SearchInput from "./SearchInput";
 import NavBar from "./NavBar";
+import { Suspense } from "react";
 
 function Header(){
     return (
-        <header className="flex flex-col gap-3 items-center p-3 xl:px-65 bg-violet-500 sticky top-0 w-full z-10">
-            <div className="w-[min(100%,40rem)]">
-                <SearchInput />
-            </div>
-            <NavBar />
-        </header>
+        <Suspense>
+            <header className="flex flex-col gap-3 items-center p-3 xl:px-65 bg-violet-500 sticky top-0 w-full z-10">
+                <div className="w-[min(100%,40rem)]">
+                    <SearchInput />
+                </div>
+                <NavBar />
+            </header>
+        </Suspense>
     );
 }
 
